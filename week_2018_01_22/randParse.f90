288d8f0ec53f18e4573dfa671540869af276a318
wolfram74 <phaugen@gmail.com>
2018-01-26 10:39:56 -0700

    setting up a random seed should not be that ridiculous

--- week_2018_01_22/randParse.f90
+++ week_2018_01_22/randParse.f90
@@ -1,24 +1,20 @@
 program randparse
   implicit none
-  real :: sample
-  integer(kind=16),dimension(1) :: seed
-  integer :: seed_size
-  ! integer(kind=16), dimension(1) :: curr_seed
-  integer(kind=4) :: curr_seed
+  integer(kind=4),dimension(12) :: new_seed
   integer :: lap
-  call random_seed(seed_size)
-  seed(1) = time()
-  call random_seed(get=curr_seed)
-  ! print *, seed_size, curr_seed
-  print *, seed_size
-  ! print *, seed
-  ! print *, size( seed)
-  ! print *, kind( seed(1))
-  ! print *, kind(seed_size)
-  ! call random_seed!(put=seed)
-  ! call random_seed(put=seed)
-  ! do lap=1,10
-  !   call random_number(sample)
-  !   print *, sample
-  ! end do
+  integer :: seed_size
+  real(kind=8) :: sample
+
+  !initializing random generator
+  call random_seed(size=seed_size)
+  do lap=1,seed_size
+    new_seed(lap)=time()
+  end do
+  call random_seed(put=new_seed)
+
+
+  do lap=1,10
+    call random_number(sample)
+    print *, sample
+  end do
 end program randparse

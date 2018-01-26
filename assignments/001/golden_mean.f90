f0f810fe5fa5862dcc663c6295fc068061c66869
wolfram74 <phaugen@gmail.com>
2018-01-26 09:11:13 -0700

    starting class

--- assignments/001/golden_mean.f90
+++ assignments/001/golden_mean.f90
@@ -4,7 +4,7 @@ program golden_mean
 
 implicit none
 integer, parameter :: dp =selected_real_kind(15,307)
-real(kind=dp)    :: phi_0, phi_1
+real(kind=dp)    :: phi_0, phi_1, actual_phi
 real(kind=dp)    :: phi_n, phi_n_minus1, phi_n_plus1
 real(kind=dp)    :: correct_answer
 integer :: n
@@ -13,8 +13,9 @@ real(kind=dp) :: deviation
 ! Initialize phi_0 and phi_1 (These are needed to start the recursion)
 ! (To calculate the square root of 5.0 use ‘sqrt(5.0) )
 phi_0 = 1.0_dp
-! phi_1 = (sqrt(5.0)-1.0_dp)/2.0_dp
-phi_1 = (1.0_dp)/1.3_dp
+actual_phi = (sqrt(5.0_dp)-1.0_dp)/2.0_dp
+! phi_1 = actual_phi
+phi_1 = (1.0_dp)/0.03_dp
 
 ! These are used initially on the RHS of the recursion formula
 phi_n_minus1 = phi_0
@@ -22,13 +23,14 @@ phi_n        = phi_1
 
 do n = 2, 100
    phi_n_plus1    = phi_n_minus1 - phi_n      ! Use Recursion Formula
-   correct_answer = (phi_1)**(n)  ! Correct answer
+   correct_answer = (actual_phi)**(n)  ! Correct answer
    deviation = (phi_n_plus1-correct_answer)/correct_answer
    ! write(*,*)  log10(abs(deviation))
    ! write(*,*)  n, phi_n_plus1, correct_answer,  log10(abs(deviation))
-   ! write(*,*)  n, phi_n/phi_n_minus1, phi_n/phi_n_minus1-phi_1
-   ! write(*,*)  n, phi_n/phi_n_minus1, (phi_n/phi_n_minus1)/phi_1
-   write(*,*)  n, phi_n/phi_n_minus1
+   write(*,*)  n, phi_n/phi_n_minus1!, phi_n/phi_n_minus1+phi_1**(-1)
+   ! write(*,*)  n, phi_n/phi_n_minus1, (phi_n/phi_n_minus1)*phi_1
+   ! write(*,*)  n, (phi_n/phi_n_minus1)**(-1)+actual_phi
+   ! write(*,*)  n, phi_n*(phi_1**n)*(-1)**n
    phi_n_minus1   = phi_n                     ! Prepare for the next recursion
    phi_n          = phi_n_plus1
 end do

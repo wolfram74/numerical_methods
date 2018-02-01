932d3f3a6b08c6c9cc9b5a31d6c51007f3aeabfb
wolfram74 <phaugen@gmail.com>
2018-02-01 14:55:25 -0700

    tests for problem 1 written and passing

--- assignments/002/secondsDecompose.f90
+++ assignments/002/secondsDecompose.f90
@@ -1,18 +1,23 @@
 program seconds_decompose
   implicit none
   print *, 'working'
-  ! call testing
-  print *, seconds_to_hms(4)
+  call testing
+  stop
   contains
+    subroutine testing()
+      print *, all([0,0,4] ==seconds_to_hms(4))
+      print *, all([1,2,4] ==seconds_to_hms(1*3600 + 2*60 + 4))
+    end subroutine
     function seconds_to_hms(seconds) result(hours_mins_secs)
       integer, dimension(3) :: hours_mins_secs
+      integer :: secs_per_hour=3600, secs_per_minute=60, remaining_secs
       integer, intent(in) :: seconds
-      hours_mins_secs = [1,1,1]
-    end function
+
+      hours_mins_secs(1) = seconds/secs_per_hour
+      remaining_secs = seconds - hours_mins_secs(1)*secs_per_hour
+      hours_mins_secs(2) = remaining_secs/secs_per_minute
+      remaining_secs = remaining_secs - hours_mins_secs(2)*secs_per_minute
+      hours_mins_secs(3) = remaining_secs
+     end function
 end program seconds_decompose
 
-! subroutine testing()
-!   print *, seconds_to_hms(4)
-!   print *, all((/1,2/) == (/1,2/))
-!   print *, all((/1,2, 3/) == (/1,1,1/))
-! end subroutine

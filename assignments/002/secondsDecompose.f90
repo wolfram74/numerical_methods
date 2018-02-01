92edd607ae7fcd889f13e6f354c7818b4124cf53
wolfram74 <phaugen@gmail.com>
2018-02-01 14:40:37 -0700

    function returns array

--- assignments/002/secondsDecompose.f90
+++ assignments/002/secondsDecompose.f90
@@ -1,9 +1,18 @@
-program secondsDecompose
-
+program seconds_decompose
+  implicit none
   print *, 'working'
-  call testing
-end program secondsDecompose
+  ! call testing
+  print *, seconds_to_hms(4)
+  contains
+    function seconds_to_hms(seconds) result(hours_mins_secs)
+      integer, dimension(3) :: hours_mins_secs
+      integer, intent(in) :: seconds
+      hours_mins_secs = [1,1,1]
+    end function
+end program seconds_decompose
 
-subroutine testing()
-  print *, all((/1,2/) == (/1,2/))
-end
+! subroutine testing()
+!   print *, seconds_to_hms(4)
+!   print *, all((/1,2/) == (/1,2/))
+!   print *, all((/1,2, 3/) == (/1,1,1/))
+! end subroutine

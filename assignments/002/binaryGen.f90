247cf7549a49d1ce3396e572b745d050be22c2c8
wolfram74 <phaugen@gmail.com>
2018-02-02 09:57:11 -0700

    splitting and base generating tests written

--- assignments/002/binaryGen.f90
+++ assignments/002/binaryGen.f90
@@ -3,10 +3,21 @@ program float_to_bin
   ! greater than 0 less than 2**16
   implicit none
   ! print *, ['0000000000001001', '1010000000000000']
-  ! print *, ['0000000000001001', '1010000000000000'] == ['0000000000001001', '1100000000000000']
+  ! print *, ( &
+  !   ['0000000000001001', '1010000000000000'] &
+  !   == ['0000000000001001', '1100000000000000']&
+  !   )
+  ! print *, size(['0000000000001001', '1010000000000000'])
+  ! print *, shape(['0000000000001001', '1010000000000000'])
 
   stop
   contains
+    function bin_generator(float_in) result(bin_vals)
+      real, intent(in) :: float_in
+      character(16), dimension(2) :: bin_vals
+
+    end function
+
     function float_splitter(float_in) result(portions)
       real, intent(in) :: float_in
       real :: portions(2)
@@ -15,6 +26,8 @@ program float_to_bin
 
     subroutine testing()
       print *, all([40., .25] == float_splitter(40.25))
-      ! print *, all([])
+      print *, all(&
+        ['0000000000001001', '1010000000000000']==&
+        bin_generator(( 8.+1.+.5 + 2.**(-3) )))
     end subroutine
 end program

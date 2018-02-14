00db4a254b30efe22b2b7f5cc365e373af86011d
wolfram74 <phaugen@gmail.com>
2018-02-14 09:12:47 -0700

    things working

---
+++ week_2018_02_05/dumby_mod.f90
@@ -0,0 +1,7 @@
+module dumby_mod
+  implicit none
+  contains
+  subroutine shout_routine()
+    print *,'fortran is cranky'
+  end subroutine shout_routine
+end module dumby_mod
---
+++ week_2018_02_05/dumby_mod_user.f90
@@ -0,0 +1,4 @@
+program dumby_mod_user
+  use dumby_mod
+  call shout_routine
+end program dumby_mod_user

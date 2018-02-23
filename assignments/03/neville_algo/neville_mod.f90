b8ba35a68f7493eb76a8fb0d3378fc18802f38c1
wolfram74 <phaugen@gmail.com>
2018-02-23 09:19:14 -0700

    test implemented

--- assignments/03/neville_algo/neville_mod.f90
+++ assignments/03/neville_algo/neville_mod.f90
@@ -3,5 +3,9 @@ module neville
   integer, parameter :: dp =selected_real_kind(15,307)
 
   contains
-
+  function neville_algo(x_vals, y_vals, query) result(guess_error)
+  real(kind=dp), intent(in) :: x_vals(:), y_vals(:), query
+  real(kind=dp) :: guess_error(2)
+  guess_error = 0
+  end function neville_algo
 end module neville
--- assignments/03/neville_algo/test_neville.f90
+++ assignments/03/neville_algo/test_neville.f90
@@ -3,7 +3,11 @@ program test_neville
   implicit none
   ! integer, parameter :: dp =selected_real_kind(15,307)
   real(kind=dp) :: epsi
-  epsi =(10_dp**(-8._dp))
+  real(kind=dp) :: testx(3), testy(3), test1(2)
+  epsi =(10_dp**(-3._dp))
   print *, 'running test cases'
-
+  testx = [-0.44_dp, 2.04_dp, 5.0_dp]
+  testy = [0.2819_dp, 2.8115_dp, 1.0_dp]
+  test1 = neville_algo(testx, testy, 3.76_dp)
+  print *, abs(test1(1)-2.3987_dp)<epsi
 end program test_neville

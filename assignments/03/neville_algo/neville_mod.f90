cffeed9e97fe6e30eb829879eae15bd9790858fe
wolfram74 <phaugen@gmail.com>
2018-02-23 11:55:49 -0700

    test probably passing

--- assignments/03/neville_algo/neville_mod.f90
+++ assignments/03/neville_algo/neville_mod.f90
@@ -4,8 +4,40 @@ module neville
 
   contains
   function neville_algo(x_vals, y_vals, query) result(guess_error)
-  real(kind=dp), intent(in) :: x_vals(:), y_vals(:), query
-  real(kind=dp) :: guess_error(2)
-  guess_error = 0
+    real(kind=dp), intent(in) :: x_vals(:), y_vals(:), query
+    real(kind=dp) :: guess_error(2)
+    real(kind=dp), allocatable :: interpolations(:, :)
+    integer :: layer, row, point_count, upper_row
+
+    allocate(interpolations(size(x_vals), size(x_vals)))
+    guess_error = 0
+    interpolations = 0
+    interpolations(1, :) = y_vals
+    point_count = size(x_vals)
+    do layer=2, point_count
+      do row=1, (point_count-layer+1)
+        upper_row = (row+layer-1)
+        print *, layer, row, upper_row
+        ! print *, interpolations(layer-1, row)
+        ! print *, interpolations(row, layer-1)
+        ! print *, (x_vals(row)-x_vals(upper_row))
+        ! print *, (query-x_vals(upper_row))
+        ! print *, (query-x_vals(upper_row))*interpolations(row, layer-1)
+        ! print *, (query-x_vals(row))*interpolations(upper_row, layer-1)
+        ! print *, (query-x_vals(upper_row))*interpolations(row, layer-1)+(query-x_vals(row))*interpolations(upper_row, layer-1)
+        interpolations(layer, row) = (&
+           (query-x_vals(upper_row))*interpolations(layer-1, row)&
+          -(query-x_vals(row))*interpolations(layer-1, row+1)&
+          )/(x_vals(row)-x_vals(upper_row))
+        ! interpolations(layer, row) = row+layer*point_count
+      end do
+    end do
+    print *, interpolations
+    guess_error(1) = interpolations(point_count,1)
+    guess_error(2) = (&
+      interpolations(point_count-1,1)&
+      -interpolations(point_count-1,2)&
+      )
+
   end function neville_algo
 end module neville
--- assignments/03/neville_algo/test_neville.f90
+++ assignments/03/neville_algo/test_neville.f90
@@ -6,8 +6,12 @@ program test_neville
   real(kind=dp) :: testx(3), testy(3), test1(2)
   epsi =(10_dp**(-3._dp))
   print *, 'running test cases'
+  ! testx = [0.1_dp, 0.2_dp, 0.3_dp]
+  ! testy = [-1.6228_dp, -0.8218_dp, -0.3027_dp]
+  ! test1 = neville_algo(testx, testy, 0.15_dp)
   testx = [-0.44_dp, 2.04_dp, 5.0_dp]
   testy = [0.2819_dp, 2.8115_dp, 1.0_dp]
   test1 = neville_algo(testx, testy, 3.76_dp)
   print *, abs(test1(1)-2.3987_dp)<epsi
+  print *, test1(2)
 end program test_neville

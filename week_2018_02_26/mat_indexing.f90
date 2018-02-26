818d3f2b2a8053739c40d04383941e0b3f47ed7b
wolfram74 <phaugen@gmail.com>
2018-02-26 10:47:02 -0700

    matrix subroutine print

--- week_2018_02_26/mat_indexing.f90
+++ week_2018_02_26/mat_indexing.f90
@@ -1,18 +1,20 @@
 program trials
   implicit none
   real, allocatable :: testing(:, :)
-  integer :: result, row_index, col_index
+  integer :: row_index, col_index
   allocate(testing(3, 4))
   testing = 0
-    do row_index=1,size(testing,1)
-      do col_index = 1, size(testing, 2)
-        testing(row_index, col_index) = col_index+(row_index-1)*(size(testing, 2))
-      end do
+  do row_index=1,size(testing,1)
+    do col_index = 1, size(testing, 2)
+      testing(row_index, col_index) = col_index+(row_index-1)*(size(testing, 2))
     end do
+  end do
 
-  result = tensor_2_printer(testing)
+  call tensor_r2_print(testing)
+  deallocate(testing)
   contains
-  function tensor_2_printer(tensor) result(output)
+
+  function tensor_r2_printer(tensor) result(output)
     real :: tensor(:, :)
     integer :: output, row_index
     output = 4
@@ -21,5 +23,18 @@ program trials
       print *, tensor(row_index, :)
     end do
   end function
+
+  subroutine tensor_r2_print(tensor)
+    real :: tensor(:, :)
+    integer :: row_index, col_index
+    do row_index=1,size(tensor,1)
+      !0.nnnE±ee
+      do col_index=1,size(tensor,2)
+        write( *, '(E9.3E2,2x)', advance="no")&
+          (tensor(row_index, col_index))
+      end do
+      write( *, *) ""
+    end do
+  end subroutine tensor_r2_print
 
 end program trials

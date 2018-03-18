a10e2b008b722eb09b2ca4c6679ab508f589cb3c
wolfram74 <phaugen@gmail.com>
2018-03-18 07:34:52 -0600

    second test written

--- assignments/04/romberg/test_romberg.f90
+++ assignments/04/romberg/test_romberg.f90
@@ -8,12 +8,20 @@ program test_romberg
   pi = 3.141592653589793_dp
   test1 = romberg_algo(test_cos, 0.0_dp, 2.0_dp*pi)
   print *, abs(test1) < epsi
+  test2 = romberg_algo(test_cubic, -7.0_dp, 7.0_dp)
+  print *, abs(test2) < epsi
 
   contains
   function test_cos(arg) result(scalar)
     real(kind=dp), intent(in) :: arg
     real(kind=dp) :: scalar
     scalar = dcos(arg)
+  end function
+
+  function test_cubic(arg) result(scalar)
+    real(kind=dp), intent(in) :: arg
+    real(kind=dp) :: scalar
+    scalar = 5.0_dp*arg**3-3.0_dp*arg
   end function
 
 end program test_romberg

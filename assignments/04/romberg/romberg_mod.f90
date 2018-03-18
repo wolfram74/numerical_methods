c980168d10d35fae501f71600376f34b8e5a7532
wolfram74 <phaugen@gmail.com>
2018-03-18 07:33:26 -0600

    first test written

--- assignments/04/romberg/romberg_mod.f90
+++ assignments/04/romberg/romberg_mod.f90
@@ -3,5 +3,10 @@ module romberg
   integer, parameter :: dp =selected_real_kind(15,307)
 
   contains
-
+  function romberg_algo(func, lower, upper) result(scalar)
+    real(kind=dp) :: func
+    real(kind=dp), intent(in) :: lower, upper
+    real(kind=dp) :: scalar
+    scalar = func(lower)+ func(upper)+4
+  end function
 end module romberg
--- assignments/04/romberg/test_romberg.f90
+++ assignments/04/romberg/test_romberg.f90
@@ -1,5 +1,19 @@
 program test_romberg
   use romberg
   implicit none
+  real(kind=dp) :: epsi, test1, test2
+  real(kind=dp) :: pi
   print *, 'running tests'
+  epsi = (10_dp**(-8._dp))
+  pi = 3.141592653589793_dp
+  test1 = romberg_algo(test_cos, 0.0_dp, 2.0_dp*pi)
+  print *, abs(test1) < epsi
+
+  contains
+  function test_cos(arg) result(scalar)
+    real(kind=dp), intent(in) :: arg
+    real(kind=dp) :: scalar
+    scalar = dcos(arg)
+  end function
+
 end program test_romberg

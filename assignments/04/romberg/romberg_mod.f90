c358e0c8d01a864d5f8cfbd33c4b168a83fb9294
wolfram74 <phaugen@gmail.com>
2018-03-20 14:04:43 -0600

    sample points and weights set up for trapezoid method

--- assignments/04/romberg/romberg_mod.f90
+++ assignments/04/romberg/romberg_mod.f90
@@ -1,3 +1,29 @@
+! romberg_algo
+  ! needs
+    ! a function, a lower and upper bound
+  ! optionally
+    ! a precision
+! R(sub_division_power,layer) == romberg with a certain number of subdivisions and at certain layer
+! sub_division_power=SDP
+! layer == L
+! R(SDP, L+1) = (4**L R(SDP, L) - R(SDP-1, L))/(4**L-1)
+! two parameters for R impact precision, larger SDP, and larger L
+! SDP >= L
+! so if L < SDP and precision not met, try increasing L
+! start by initializing a 20x20 array for values of R(SDP, L)
+! call array approximations
+! R(20,20) if i'm interpretting this correctly would have a precision vastly smaller than floating point, so no point going past.
+! loop from 0 to 20 for SDP
+!   loop from 0 to SDP for L
+!     if L = 0, then approximations(SDP, 0) = trapezoid(SDP, lower, upper, func)
+!     else use recurrence relation to find next next value
+!     if L === SDP do exit check
+!
+!
+!
+!
+
+
 module romberg
   implicit none
   integer, parameter :: dp =selected_real_kind(15,307)
@@ -7,6 +33,28 @@ module romberg
     real(kind=dp) :: func
     real(kind=dp), intent(in) :: lower, upper
     real(kind=dp) :: scalar
+    real(kind=dp) :: precision=(10_dp**(-3._dp))
+
+    scalar = func(lower)+ func(upper)+4
+  end function
+
+  function trapezoid_algo(func, lower, upper, resolution) result(scalar)
+    real(kind=dp) :: func
+    real(kind=dp), intent(in) :: lower, upper
+    integer :: resolution
+    real(kind=dp) :: scalar
+    real(kind=dp) :: x_spacing
+    integer :: sample_points, i
+    real(kind=dp), dimension(:),allocatable :: weights, sample_xs
+
+    sample_points = 2**resolution
+    x_spacing = (upper-lower)/(2**resolution)
+    allocate(sample_xs (0:sample_points))
+    allocate(weights (0:sample_points))
+    sample_xs = x_spacing*(/(i, i=0,sample_points, 1)/)+lower
+    weights = 2.0_dp
+    weights(0) = weights(0)-1.0_dp
+    weights(sample_points) = weights(sample_points)-1.0_dp
     scalar = func(lower)+ func(upper)+4
   end function
 end module romberg
--- assignments/04/romberg/test_romberg.f90
+++ assignments/04/romberg/test_romberg.f90
@@ -1,16 +1,25 @@
 program test_romberg
   use romberg
   implicit none
-  real(kind=dp) :: epsi, test1, test2
-  real(kind=dp) :: pi
+  real(kind=dp) :: epsi, test1, test2, test3, result3
+  real(kind=dp) :: ttest1, ttest2
+  real(kind=dp) :: pi, e
   print *, 'running tests'
   epsi = (10_dp**(-8._dp))
   pi = 3.141592653589793_dp
+  e = 2.718281828459045_dp
+  result3 = e*(5.0_dp*e**2.0_dp-1.0_dp)
   test1 = romberg_algo(test_cos, 0.0_dp, 2.0_dp*pi)
   print *, abs(test1) < epsi
   test2 = romberg_algo(test_cubic, -7.0_dp, 7.0_dp)
   print *, abs(test2) < epsi
-
+  test3 = romberg_algo(test_wonky, 1.0_dp, 3.0_dp)
+  print *, abs( &
+    test3-result3&
+    ) < epsi
+  ttest1 = trapezoid_algo(test_wonky, 1.0_dp, 3.0_dp, 3)
+  ttest2 = trapezoid_algo(test_wonky, 1.0_dp, 3.0_dp, 5)
+  print *, abs(ttest2-result3) < abs(ttest1-result3)
   contains
   function test_cos(arg) result(scalar)
     real(kind=dp), intent(in) :: arg
@@ -22,6 +31,12 @@ program test_romberg
     real(kind=dp), intent(in) :: arg
     real(kind=dp) :: scalar
     scalar = 5.0_dp*arg**3-3.0_dp*arg
+  end function
+
+  function test_wonky(arg) result(scalar)
+    real(kind=dp), intent(in) :: arg
+    real(kind=dp) :: scalar
+    scalar = (arg**2)*(e**(arg))
   end function
 
 end program test_romberg

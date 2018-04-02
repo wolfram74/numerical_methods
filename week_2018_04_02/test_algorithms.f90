bb939209ca9fe97799b4ac20ddc1fa0b9f9eb26a
wolfram74 <phaugen@gmail.com>
2018-04-02 10:12:03 -0600

    compiling, undefined error

--- week_2018_04_02/test_algorithms.f90
+++ week_2018_04_02/test_algorithms.f90
@@ -4,13 +4,14 @@ program test_algorithms
   ! use assets
   implicit none
   real(kind=dp) :: length
-  print *, dp
   type(body2d) :: testBody
+  print *, dp
   testBody%mass = 0.0_dp
   testBody%location = [5.0_dp, 0.0_dp]
   testBody%velocity = [0.0_dp, 0.0_dp]
   length = testBody%distance(nullBody)
   print *, length
+  print *, nullBody%mass
   contains
 end program test_algorithms
 

0cb808fa2954d6b5ff932d7acc7ddba39678d6d9
wolfram74 <phaugen@gmail.com>
2018-04-02 10:07:47 -0600

    can't define testBody

--- week_2018_04_02/assets_mod.f90
+++ week_2018_04_02/assets_mod.f90
@@ -1,10 +1,31 @@
 
 module assets
-    implicit none
-    integer, parameter :: dp =selected_real_kind(15,307)
-    type body
+  implicit none
+  integer, parameter :: dp =selected_real_kind(15,307)
 
-    end type body
+  type body2d
+    real(kind=dp) :: mass, location(2), velocity(2)
     contains
+    procedure :: distance
+  end type body2d
+  type(body2d), parameter :: nullBody= body2d(&
+    0.0_dp,&
+    [0.0_dp, 0.0_dp], &
+    [0.0_dp, 0.0_dp] )
+  ! nullBody%mass =
+  ! nullBody%location =
+  ! nullBody%velocity =
+
+  contains
+
+  function distance(body1, body2) result(scalar)
+    class(body2d), intent(in):: body1, body2
+    real(kind=dp) :: scalar
+    scalar = 0
+    scalar = (body1%location(1)-body2%location(1))**2
+    scalar = scalar + (body1%location(2)-body2%location(2))**2
+    scalar = scalar**(0.5_dp)
+  end function
+
 end module assets
 
--- week_2018_04_02/test_algorithms.f90
+++ week_2018_04_02/test_algorithms.f90
@@ -1,8 +1,16 @@
 
 program test_algorithms
-    use algorithms
-    implicit none
-    print *, dp
-    contains
+  use algorithms
+  ! use assets
+  implicit none
+  real(kind=dp) :: length
+  print *, dp
+  type(body2d) :: testBody
+  testBody%mass = 0.0_dp
+  testBody%location = [5.0_dp, 0.0_dp]
+  testBody%velocity = [0.0_dp, 0.0_dp]
+  length = testBody%distance(nullBody)
+  print *, length
+  contains
 end program test_algorithms
 

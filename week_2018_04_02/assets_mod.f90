a7a5249b5ace9a323e32289f44a094c8baf3687f
wolfram74 <phaugen@gmail.com>
2018-04-04 09:21:44 -0600

    trying to make a function an attribute

--- week_2018_04_02/assets_mod.f90
+++ week_2018_04_02/assets_mod.f90
@@ -5,8 +5,10 @@ module assets
 
   type body2d
     real(kind=dp) :: mass, location(2), velocity(2)
+    procedure :: interaction
     contains
     procedure :: distance => distanceIsolated
+    procedure :: force => forceIsolated
   end type body2d
   type(body2d), parameter :: nullBody= body2d(&
     0.0_dp,&
@@ -21,6 +23,15 @@ module assets
     scalar = &
       sum((body1%location-body2%location)**2)**(0.5_dp)
   end function
+
+  function forceIsolated(body1, body2) result(vector)
+    class(body2d), intent(in) :: body1, body2
+    real(kind=dp) :: vector(2)
+    real(kind=dp) :: distance
+    distance = body1%distance(body2)
+    vector = (body1%location-body2%location)/distance
+  end function
+
 
 end module assets
 
--- week_2018_04_02/test_algorithms.f90
+++ week_2018_04_02/test_algorithms.f90
@@ -3,16 +3,24 @@ program test_algorithms
   use algorithms
   ! use assets
   implicit none
-  real(kind=dp) :: length
+  real(kind=dp) :: length, direction(2)
   type(body2d) :: testBody
   print *, dp
   print *, nullBody%mass
   testBody%mass = 0.0_dp
   testBody%location = [3.0_dp, 4.0_dp]
   testBody%velocity = [0.0_dp, 0.0_dp]
+  testBody%interaction = harmonic
   length = testBody%distance(nullBody)
+  direction = testBody%force(nullBody)
   ! length = distanceIsolated(testBody, nullBody)
   print *, length
+  print *, direction
   contains
+  function harmonic(body1, body2) result(vector)
+    class(body2d), intent(in):: body1, body2
+    real(kind=dp) :: vector(2)
+    vector = 0
+  end function
 end program test_algorithms
 

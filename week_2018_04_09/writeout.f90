3ac95161b1cd3e0e6833bf2c42df18a0d2832b30
wolfram74 <phaugen@gmail.com>
2018-04-09 12:51:36 -0600

    retweaked writeout function

--- week_2018_04_09/writeout.f90
+++ week_2018_04_09/writeout.f90
@@ -1,30 +1,28 @@
 program main_project
     implicit none
-    integer(kind=4) :: timeStamp, result
+    integer(kind=4) ::  result
     real(kind=8) :: sample(4, 3)
-    timeStamp = time()
     sample(1, :) = [1.0, 2.0, 3.0]
     sample(2, :) = [2.0, 5.0, 3.0]
     sample(3, :) = [3.0, 8.0, 3.0]
     sample(4, :) = [4.0, 11.0, 3.0]
-    print*, timeStamp
-    ! print*, sample
-    result = whiteOut(sample, timeStamp)
+    result = writeOutAtTime(sample)
     contains
-    function whiteOut(dataVals, fileNumber) result(status)
+    function writeOutAtTime(dataVals) result(status)
       real(kind=8), intent(in) :: dataVals(:, :)
-      integer(kind=4), intent(in) :: fileNumber
+      integer(kind=4) :: fileNumber
       character(len=20) :: fileName
       integer :: status, lineCount
+      fileNumber = time()
       write(fileName, '(i10, a4)') fileNumber, '.txt'
-      ! print*, trim(fileName)
+      print*, trim(fileName)
       open(1, file=fileName)
       do lineCount=1,size(dataVals,1)
-        print *, lineCount, dataVals(lineCount, :)
+        ! print *, lineCount, dataVals(lineCount, :)
         write(1, *) dataVals(lineCount, :)
       end do
       close(1)
       status = 4
-    end function whiteOut
+    end function writeOutAtTime
 end program main_project
 

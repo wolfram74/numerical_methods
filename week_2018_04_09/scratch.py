1d92988e8b2247691de33304bcc9090a48806870
wolfram74 <phaugen@gmail.com>
2018-04-11 13:05:25 -0600

    interesting results on energy error of verlet ODE solver for potential

--- week_2018_04_09/scratch.py
+++ week_2018_04_09/scratch.py
@@ -1,12 +1,25 @@
 import sympy
 
+def taylor(expr, vari, terms):
+    return sum(
+        [
+        expr.diff(
+            vari, i
+            ).subs(
+            vari, 0
+            )*(
+            vari**i/sympy.factorial(i)
+            ) for i in xrange(0,terms)
+        ]
+        )
+
 def euler_err():
     v, x = sympy.symbols('v x', real=True)
     h = sympy.symbols('h', real=True, positive=True)
     u = x**2/2
-    # u = sympy.exp(-x**2)
+    u = sympy.exp(-x**2)
     a = -u.diff(x)
-    # maxA = sympy.solve(a.diff(x), x)[1]
+    maxA = sympy.solve(a.diff(x), x)[1]
     te1 = u+v**2/2
     x2 = x+v*h
     v2 = v+a*h
@@ -17,7 +30,17 @@ def euler_err():
     print('difference')
     discrep = (te2-te1)
     sympy.pprint(discrep)
-    sympy.pprint(discrep.expand(h).collect(h))
+    h_terms = discrep.expand(h).collect(h)
+    gauss_terms = discrep.expand(h).collect(u)
+    # sympy.pprint(h_terms)
+    # sympy.pprint(gauss_terms)
+    # sympy.pprint(h_terms.coeff(h, 0))
+    # sympy.pprint(gauss_terms.coeff(u, 1))
+    dominant = gauss_terms.coeff(u, 1)
+    approx = taylor(dominant, h, 4)+gauss_terms-dominant*u
+
+    sympy.pprint(approx.collect(h))
+    # sympy.pprint(approx.simplify())
     try:
         maxA
     except NameError:
@@ -25,7 +48,7 @@ def euler_err():
     if maxA != None:
         print('discrep at max acceleration')
         sympy.pprint(maxA)
-        sympy.pprint(discrep.expand(h).subs(x, maxA))
+        sympy.pprint(approx.expand(h).subs(x, maxA))
     # sympy.pprint(discrep.diff(x))
     # sympy.pprint(discrep.diff(x).subs(x, maxA))
     # sympy.pprint(discrep.diff(x).subs(x, 0))
@@ -42,12 +65,12 @@ def euler_err():
 def verlet_err():
     v, x = sympy.symbols('v x', real=True)
     h = sympy.symbols('h', real=True, positive=True)
-    u = x**2/2
-    # u = sympy.exp(-x**2)
+    # u = x**2/2
+    u = sympy.exp(-x**2)
     a = -u.diff(x)
-    # maxA = sympy.solve(a.diff(x), x)[1]
+    maxA = sympy.solve(a.diff(x), x)[1]
     te1 = u+v**2/2
-    x2 = x+v*h+a*h**2/2
+    x2 = x + v*h + a*h**2/2
     v2 = v+(a+a.subs(x, x2))*h/2
     te2 = u.subs(x, x2) + v2**2/2
     print('energy expressions')
@@ -55,8 +78,14 @@ def verlet_err():
     sympy.pprint(te2)
     print('difference')
     discrep = (te2-te1)
-    sympy.pprint(discrep)
-    sympy.pprint(discrep.expand(h).collect(h))
+    # sympy.pprint(discrep)
+    # sympy.pprint(discrep.expand(h).collect(h))
+    gauss_terms = discrep.expand().collect(u)
+    # sympy.pprint(gauss_terms)
+    approx = taylor(gauss_terms, h, 4)
+    sympy.pprint(approx)
+    cubic_coeff=approx.coeff(h, 3).expand().collect(u)
+    sympy.pprint(cubic_coeff)
     try:
         maxA
     except NameError:
@@ -64,10 +93,10 @@ def verlet_err():
     if maxA != None:
         print('discrep at max acceleration')
         sympy.pprint(maxA)
-        sympy.pprint(discrep.expand(h).subs(x, maxA).collect(h))
+        sympy.pprint(cubic_coeff.subs(x, maxA))
 
     return
 
 if __name__ == '__main__':
-    euler_err()
-    # verlet_err()
+    # euler_err()
+    verlet_err()

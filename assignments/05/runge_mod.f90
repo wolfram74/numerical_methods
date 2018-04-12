a4a0c21baf6f6dda371fdc6b06a354c43e9ff4d8
wolfram74 <phaugen@gmail.com>
2018-04-12 14:11:40 -0600

    passing one test

--- assignments/05/runge_mod.f90
+++ assignments/05/runge_mod.f90
@@ -14,8 +14,27 @@ module runge
     real(kind=dp), intent(in) :: state(:)
     real(kind=dp), intent(in) :: stepSize
     real(kind=dp), allocatable :: stateStep(:)
-    allocate(stateStep(size(state)))
-    stateStep = 0.0_dp
+    real(kind=dp), allocatable :: kernel1(:), kernel2(:),&
+                                  kernel3(:), kernel4(:)
+    integer :: stateSize
+    stateSize = size(state)
+    allocate(stateStep(stateSize) )
+    allocate(kernel1(stateSize))
+    allocate(kernel2(stateSize))
+    allocate(kernel3(stateSize))
+    allocate(kernel4(stateSize))
+    kernel1 = stepSize * gradFunc(state)
+    kernel2 = stepSize * gradFunc(state+kernel1*0.5_dp)
+    kernel3 = stepSize * gradFunc(state+kernel2*0.5_dp)
+    kernel4 = stepSize * gradFunc(state+kernel3)
+    stateStep = state + (6.0_dp**(-1))*(&
+      kernel1 +&
+      2.0_dp * kernel2 +&
+      2.0_dp * kernel3 +&
+      kernel4 &
+      )
+    ! stateStep = 0.0_dp
+
   end function rk4Step
 end module runge
 
